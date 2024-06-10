2109a568630bd533774b106c74899e15338ccf55
Yann Zhang <yz.yannzhang@gmail.com>
2024-06-10 17:53:49 +0200

    problem 007

---
+++ euler007.py
@@ -0,0 +1,28 @@
+# 10001st Prime
+
+from math import isqrt
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for k in range(2, isqrt(n) + 1):
+        if n % k == 0:
+            return False
+    return True
+
+def find_prime():
+    count = 0
+    n = 2
+    primes = []
+
+    while len(primes) < 10001:
+        if is_prime(n) == True:
+            primes.append(n)
+        n += 1
+    
+    return primes[-1]
+
+print(find_prime())
+
+
+    
